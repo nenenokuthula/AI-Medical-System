e962a7927e302222af41db20c968af052fd8a110
nenenokuthula <nenenokuthula45@gmail.com>
2025-05-04 22:54:01 +0200

    initial commit

---
+++ Service Layer and REST API/src/LibraryApplication.java
@@ -0,0 +1,8 @@
+package com.library;
+
+public class LibraryApplication {
+    public static <InnocuousThread> void main(String[] args) {
+        InnocuousThread SpringApplication = null;
+
+    }
+}
